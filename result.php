a4fd18ab8bb2062e7213e0cb10e8a670d17b0a5b
Sachin Timilsinia <157892400+SachinGuy@users.noreply.github.com>
2024-03-02 10:59:47 +0545

    Commited new files

---
+++ result.php
@@ -0,0 +1,45 @@
+<?php
+require_once ("./templates/header.php");
+
+if(! playersRegistered())
+{
+    header("location: index.php");
+}
+
+resetBoard();
+?>
+
+<table class="wrapper" cellpadding = "0" cellspacing = "0">
+    <tr>
+        <td>
+            <div class="welcome">
+                <h1>
+                    <?php 
+                    if($_GET["player"])
+                    {
+                        echo currentPlayer() . " won!";
+                    }
+                    else 
+                    {
+                        echo "It's a tie!";    
+                    }
+                    ?>
+                </h1>
+
+                <div class="player-name">
+                    <?php echo playerName('x')?>'s score: <b><?php echo score('x')?></b>
+                </div>
+
+                <div class="player-name">
+                    <?php echo playerName('o')?>'s score: <b><?php echo score('o')?></b>
+                </div>
+
+                <a href = "play.php"> Play Again </a> <br>
+                
+                <a href = "index.php" class="reset-btn">Reset</a> 
+            </div>
+        </td>  
+    </tr>
+</table>
+
+<?php require_once("./templates/footer.php")?>
